996306e7d2204d6928bf5b8948e159061691bffc
JukePenguin <JukePenguin@DESKTOP-3GNOKES>
2019-02-16 18:04:02 -0800

    Sending over solution.

---
+++ WebApplication1/Controllers/HWController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HWController : Controller
+    {
+        // GET: HW
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+        
+    }
+}
